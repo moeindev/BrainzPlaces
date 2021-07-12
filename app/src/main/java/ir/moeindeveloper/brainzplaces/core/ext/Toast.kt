d1bdc4b5505ba36896cf5b6becf3841f11ecac6f
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-07-12 10:30:13 +0430

    feat(core): Write Toast EXT functions

---
+++ app/src/main/java/ir/moeindeveloper/brainzplaces/core/ext/Toast.kt
@@ -0,0 +1,18 @@
+package ir.moeindeveloper.brainzplaces.core.ext
+
+import android.content.Context
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+
+
+fun Context.shortToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+}
+
+fun Context.longToast(message: String) {
+    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+}
+
+fun Fragment.shortToast(message: String) = requireContext().shortToast(message)
+
+fun Fragment.longToast(message: String) = requireContext().longToast(message)
