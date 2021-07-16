42d3e972c610a805e69978337e33587bb9903570
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-07-16 17:12:03 +0430

    feat(places): Write PlaceRepository #3

---
+++ app/src/main/java/ir/moeindeveloper/brainzplaces/places/repository/PlaceRepository.kt
@@ -0,0 +1,15 @@
+package ir.moeindeveloper.brainzplaces.places.repository
+
+import ir.moeindeveloper.brainzplaces.core.state.UiState
+import ir.moeindeveloper.brainzplaces.places.entity.Place
+import kotlinx.coroutines.flow.Flow
+
+
+interface PlaceRepository {
+
+    /**
+     * Search places using
+     */
+    fun searchPlaces(query: String): Flow<UiState<ArrayList<Place>>>
+
+}
