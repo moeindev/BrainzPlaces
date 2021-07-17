f4bb790ce1ae4531758ff15762abe03dcd1f743a
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-07-17 20:54:53 +0430

    feat(places): Write places Repository's test #3 & #6

---
+++ app/src/test/java/ir/moeindeveloper/brainzplaces/repository/PlaceRepositoryTest.kt
@@ -0,0 +1,88 @@
+package ir.moeindeveloper.brainzplaces.repository
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import com.skydoves.sandwich.ApiResponse
+import io.kotest.matchers.shouldBe
+import ir.moeindeveloper.brainzplaces.MainCoroutinesRule
+import ir.moeindeveloper.brainzplaces.core.state.UiState
+import ir.moeindeveloper.brainzplaces.mocks.PlacesApiResponse
+import ir.moeindeveloper.brainzplaces.places.entity.Place
+import ir.moeindeveloper.brainzplaces.places.repository.PlaceRepository
+import ir.moeindeveloper.brainzplaces.places.repository.PlaceRepositoryImpl
+import ir.moeindeveloper.brainzplaces.places.service.PlaceService
+import ir.moeindeveloper.brainzplaces.toPlaceResponse
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import retrofit2.Response
+import kotlin.time.ExperimentalTime
+
+@ExperimentalTime
+@ExperimentalCoroutinesApi
+class PlaceRepositoryTest {
+
+    private val service: PlaceService = mock()
+    private lateinit var repository: PlaceRepository
+
+    @get:Rule
+    var coroutinesRule = MainCoroutinesRule()
+
+    @get:Rule
+    var instantExecutor = InstantTaskExecutorRule()
+
+    @Before
+    fun initialSetup() {
+        repository = PlaceRepositoryImpl(service)
+    }
+
+    //Currently this is failing but I don't have time to make a JSON mock :)
+    @Test
+    fun `Big data response`() = runBlocking {
+        val queryResult = ApiResponse.Success(response = Response.success(PlacesApiResponse.longRange.toPlaceResponse()))
+
+        val expectedCount = 52
+
+        whenever(service.searchPlaces(
+            "ok"
+        )).thenReturn(queryResult)
+
+        runQueryToTest("ok",expectedCount)
+    }
+
+    //Same as above :)
+    @Test
+    fun `Average data response`() = runBlocking {
+        val queryResult = ApiResponse.Success(response = Response.success(PlacesApiResponse.averageRange.toPlaceResponse()))
+
+        val expectedCount = 28
+
+        whenever(service.searchPlaces(
+            "god"
+        )).thenReturn(queryResult)
+
+        runQueryToTest("god",expectedCount)
+    }
+
+    @Test
+    fun `small data response`() = runBlocking {
+        val queryResult = ApiResponse.Success(response = Response.success(PlacesApiResponse.smallRange.toPlaceResponse()))
+
+        val expectedCount = 7
+
+        whenever(service.searchPlaces(
+            "iran"
+        )).thenReturn(queryResult)
+
+        runQueryToTest("iran",expectedCount)
+    }
+
+
+    private fun runQueryToTest(query: String, count: Int) = runBlocking {
+        val places = repository.searchPlaces(query)
+        places.data?.size shouldBe count
+    }
+}
