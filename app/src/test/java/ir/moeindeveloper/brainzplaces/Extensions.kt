d5846836581ff36fa163fabfd485b69b95955a34
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-07-17 20:47:37 +0430

    feat(places): Extension function to test placesResponse #3 & #6

--- app/src/test/java/ir/moeindeveloper/brainzplaces/Extensions.kt
+++ app/src/test/java/ir/moeindeveloper/brainzplaces/Extensions.kt
@@ -1,8 +1,17 @@
 package ir.moeindeveloper.brainzplaces
 
+import com.google.gson.Gson
+import ir.moeindeveloper.brainzplaces.places.entity.PlacesResponse
+
 inline fun <T> T.assertThat(block: T.() -> Unit) = block()
 
 inline fun <T> Iterable<T>.assertForeach(block: T.(position: Int) -> Unit) =
     forEachIndexed { index, item ->
         item.run { block(index) }
     }
+
+fun String.toPlaceResponse(): PlacesResponse? {
+    val gson = Gson()
+
+    return gson.fromJson(this, PlacesResponse::class.java)
+}
