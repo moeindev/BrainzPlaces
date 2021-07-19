0989fbac1d33533b7a983e85782cd675b0f7bd58
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-07-19 16:34:39 +0430

    feat(places): use base activity and dataBinding

--- app/src/main/java/ir/moeindeveloper/brainzplaces/MainActivity.kt
+++ app/src/main/java/ir/moeindeveloper/brainzplaces/MainActivity.kt
@@ -1,13 +1,10 @@
 package ir.moeindeveloper.brainzplaces
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import dagger.hilt.android.AndroidEntryPoint
+import ir.moeindeveloper.brainzplaces.core.platform.activity.BaseActivity
+import ir.moeindeveloper.brainzplaces.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun layoutRes(): Int = R.layout.activity_main
 }
