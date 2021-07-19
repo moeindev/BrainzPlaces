277912a264ffa294f8c1ff952f63027a68435c7e
Mohammad Moein Abdi <moeinabdi77@gmail.com>
2021-07-20 00:17:38 +0430

    feat(places): place extension functions for google maps #3

--- app/src/main/java/ir/moeindeveloper/brainzplaces/places/Extension.kt
+++ app/src/main/java/ir/moeindeveloper/brainzplaces/places/Extension.kt
@@ -1,6 +1,10 @@
 package ir.moeindeveloper.brainzplaces.places
 
 import android.os.Build
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.skydoves.whatif.whatIfNotNull
+import ir.moeindeveloper.brainzplaces.places.entity.Place
 import java.time.LocalDate
 import java.util.*
 
@@ -29,3 +33,46 @@ fun getCurrentYear(): Int {
 }
 
 fun String.buildQuery(): String = "$this ANDbegin:\"1990-01-01\""
+
+fun Place.latLng(): LatLng? {
+    return if (coordinates != null) {
+        return if (coordinates.latitude != null && coordinates.longitude != null) {
+            LatLng(coordinates.latitude.toDouble(), coordinates.longitude.toDouble())
+        } else {
+            null
+        }
+    } else {
+        null
+    }
+}
+
+fun Place.toMarkerOptions(): MarkerOptions? {
+    return if (latLng() != null) {
+        MarkerOptions()
+            .position(
+                latLng()!!
+            ).title(name)
+    } else {
+        null
+    }
+}
+
+
+fun Place.markerTag(): String = id ?: name ?: "nullTag"
+
+
+fun List<Place>.getAverageCoordinates(): LatLng {
+    var sumOfAllLat = 0.0
+    var sumOfAllLng = 0.0
+
+    forEach { place ->
+        place.latLng().whatIfNotNull { ltLn ->
+            sumOfAllLat += ltLn.latitude
+            sumOfAllLng += ltLn.longitude
+        }
+    }
+
+    return LatLng(
+        sumOfAllLat/size, sumOfAllLng/size
+    )
+}
